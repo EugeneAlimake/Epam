9d4f1bc7c84aec1de552692e9fe1a261ced7c838
Eugene <90150437+EugeneAlimake@users.noreply.github.com>
2022-11-18 18:55:49 +0300

    Add files via upload

---
+++ 6 lab/Basic of .Net Fremework/Basic of .Net Fremework/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Basic_of_.Net_Fremework
+{
+    class Program:Unit
+    {
+        static void Main(string[] args)
+        {
+            int n, x, i;
+            do Console.Write("Число:");
+            while (!int.TryParse(Console.ReadLine(), out n));
+            do Console.Write("Пропишите исчислительную систему(от 2 до 20):");
+            while (!int.TryParse(Console.ReadLine(), out i));
+            while(i<2||i>20)
+            {
+                Console.Write("Пропишите правильно исчислительную систему:");
+                int.TryParse(Console.ReadLine(), out i);
+            }
+            x = n;
+                string s = n == 0 ? "0" : "";
+                while (x > 0)
+                {
+                    s = x % i + s;
+                    x /= i;
+                }
+                Console.WriteLine("{0}-я:{1}", i, s);
+            Console.ReadKey();
+            string str = "aaaabbaaccde";
+            int[] a = new int[10] { 1, 2, 3, 5 ,5 ,5 ,3 ,4 ,5 ,4 };
+            Console.Write(maxRepeatingint(a));
+            Console.Write(maxRepeating(str));
+            Console.Write(maxnoRepeating(str));
+        }
+
+    }
+}
---
+++ 6 lab/Basic of .Net Fremework/Basic of .Net Fremework/Unit.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_of_.Net_Fremework
+{
+    public class Unit
+    {
+
+        public static char maxnoRepeating(string str)
+        {
+            int len = str.Length;
+            int count = 0;
+            char res = str[0];
+
+            // Find the maximum repeating
+            // character starting from str[i]
+            for (int i = 0; i < len; i++)
+            {
+                int cur_count = 1;
+                for (int j = i + 1; j < len; j++)
+                {
+                    if (str[i] != str[j])
+                        cur_count++;
+                    else
+                        break;
+                }
+
+                // Update result if required
+                if (cur_count > count)
+                {
+                    count = cur_count;
+                    res = str[i];
+                }
+            }
+            return res;
+        }
+        public static char maxRepeating(string str)
+        {
+            int len = str.Length;
+            int count = 0;
+            char res = str[0];
+
+            for (int i = 0; i < len; i++)
+            {
+                int cur_count = 1;
+                for (int j = i + 1; j < len; j++)
+                {
+                    if (str[i] != str[j])
+                        break;
+                    cur_count++;
+                }
+
+                if (cur_count > count)
+                {
+                    count = cur_count;
+                    res = str[i];
+                }
+            }
+            return res;
+        }
+
+        public static int maxRepeatingint(int[] a)
+        {
+
+            int n = 10;
+            int x = a[0], cnt = 1, mx = -1;
+            for (int i = 1; i < n; ++i)
+            {
+                if (a[i] == x)
+                    cnt++;
+                else
+                {
+                    if (cnt > mx)
+                        mx = cnt;
+                    cnt = 1;
+                    x = a[i];
+                }
+            }
+            if (cnt > mx)
+                mx = cnt;
+            return mx;
+        }
+    }
+}
