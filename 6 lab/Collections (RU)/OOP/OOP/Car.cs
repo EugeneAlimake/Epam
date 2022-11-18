75764d4364044ec2e8a92d7c5a725e70080c8006
Eugene <90150437+EugeneAlimake@users.noreply.github.com>
2022-11-18 18:54:30 +0300

    Add files via upload

---
+++ 6 lab/Collections (RU)/OOP/OOP/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+     class Car
+    {
+        public string _Type;
+        public Engine _engine;
+        public Chassis _chassis;
+        public Transmission _transmission;
+
+        public Car()
+        {
+
+        }
+        public Car(string type,Engine engine, Chassis chassis, Transmission transmission)
+        {
+            _Type = type;
+            _engine = engine;
+            _chassis = chassis;
+            _transmission = transmission;
+
+        }
+        public void Print()
+        {
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.WriteLine($"Тип: {_Type}, Хар-ки:");
+            _engine.Print();
+            _chassis.Print();
+            _transmission.Print();
+            Console.WriteLine("---------------------------------------------------------------------");
+        }
+    }
+}
